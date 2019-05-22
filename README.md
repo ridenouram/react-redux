17c1c259394c46b1ab2254ae95191106995be316
Anna Ridenour <annamridenour@gmail.com>
2019-05-22 16:48:44 -0700

    moved reducer to own directory

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Refactor your redux-vanilla-lunch extracting actions into their own directory
+Refactor your redux-vanilla-lunch extracting reducer into its own directory
--- src/index.js
+++ src/index.js
@@ -1,46 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
-import { ADD_DRINK, addDrink, REMOVE_DRINK, removeDrink, EMPTY_DRINKS, emptyDrinks } from './actions/drinksActions';
-import { ADD_CHIPS, REMOVE_CHIPS, EMPTY_CHIPS, addChips, removeChips, emptyChips } from './actions/chipsActions';
-import { ADD_SANDWICH, REMOVE_SANDWICH, EMPTY_SANDWICHES, addSandwich, removeSandwich, emptySandwiches } from './actions/sandwichActions';
-
-const initialState = {
-  drinks: [],
-  chips: [],
-  sandwiches: []
-};
-
-function remove(array, element) {
-  const index = array.indexOf(element);
-  array.splice(index, 1);
-  return array;
-}
-
-function reducer(state = initialState, action) {
-  switch(action.type) {
-    case ADD_DRINK:
-      return { ...state, drinks: [...state.drinks, action.payload] };
-    case ADD_CHIPS:
-      return { ...state, chips: [...state.chips, action.payload] };
-    case ADD_SANDWICH:
-      return { ...state, sandwiches: [...state.sandwiches, action.payload] };
-    case REMOVE_DRINK:
-      return { ...state, drinks: remove(state.drinks, action.payload) };
-    case REMOVE_CHIPS:
-      return { ...state, chips: remove(state.chips, action.payload) };
-    case REMOVE_SANDWICH:
-      return { ...state, sandwiches: remove(state.sandwiches, action.payload) };
-    case EMPTY_DRINKS: 
-      return { ...state, drinks: [] };
-    case EMPTY_CHIPS: 
-      return { ...state, chips: [] };
-    case EMPTY_SANDWICHES: 
-      return { ...state, sandwiches: [] };
-    default:
-      return state;
-  }
-}
+import { addDrink, removeDrink, emptyDrinks } from './actions/drinksActions';
+import { addChips, removeChips, emptyChips } from './actions/chipsActions';
+import { addSandwich, removeSandwich, emptySandwiches } from './actions/sandwichActions';
+import reducer from './reducers/lunchReducer';
 
 const store = createStore(reducer);
 
