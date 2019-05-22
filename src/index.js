d71aae26ed678015695b03fc90c7844fcd9799fc
Anna Ridenour <annamridenour@gmail.com>
2019-05-22 14:34:05 -0700

    set initalState, made reducer with add/remove actions for drink, chips, and sandwich

--- src/index.js
+++ src/index.js
@@ -1,6 +1,32 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
+import { createStore } from 'redux';
+
+const initialState = {
+  drink: null,
+  chips: null,
+  sandwich: null
+};
+
+function reducer(state = initialState, action) {
+  switch(action.type) {
+    case 'ADD_DRINK':
+      return { ...state, drink: action.payload };
+    case 'ADD_CHIPS':
+      return { ...state, chips: action.payload };
+    case 'ADD_SANDWICH':
+      return { ...state, sandwich: action.payload };
+    case 'REMOVE_DRINK':
+      return { ...state, drink: null };
+    case 'REMOVE_CHIPS':
+      return { ...state, chips: null };
+    case 'REMOVE_SANDWICH':
+      return { ...state, sandwich: null };
+    default:
+      return state;
+  }
+}
 
 render(
   <App />,
