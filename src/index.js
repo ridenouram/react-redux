0c83d7aa8f7b3cabeca365f17de0314d1bf655d2
Anna Ridenour <annamridenour@gmail.com>
2019-05-22 15:48:00 -0700

    moved chipsActions, refactored index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { render } from 'react-dom';
 import { createStore } from 'redux';
 import './actions/drinksActions';
 import { ADD_DRINK, addDrink, REMOVE_DRINK, removeDrink, EMPTY_DRINKS, emptyDrinks } from './actions/drinksActions';
+import './actions/chipsActions';
+import { ADD_CHIPS, REMOVE_CHIPS, EMPTY_CHIPS, addChips, removeChips, emptyChips } from './actions/chipsActions';
 
 const initialState = {
   drinks: [],
@@ -20,19 +22,19 @@ function reducer(state = initialState, action) {
   switch(action.type) {
     case ADD_DRINK:
       return { ...state, drinks: [...state.drinks, action.payload] };
-    case 'ADD_CHIPS':
+    case ADD_CHIPS:
       return { ...state, chips: [...state.chips, action.payload] };
     case 'ADD_SANDWICH':
       return { ...state, sandwiches: [...state.sandwiches, action.payload] };
     case REMOVE_DRINK:
       return { ...state, drinks: remove(state.drinks, action.payload) };
-    case 'REMOVE_CHIPS':
+    case REMOVE_CHIPS:
       return { ...state, chips: remove(state.chips, action.payload) };
     case 'REMOVE_SANDWICH':
       return { ...state, sandwiches: remove(state.sandwiches, action.payload) };
     case EMPTY_DRINKS: 
       return { ...state, drinks: [] };
-    case 'EMPTY_CHIPS': 
+    case EMPTY_CHIPS: 
       return { ...state, chips: [] };
     case 'EMPTY_SANDWICHES': 
       return { ...state, sandwiches: [] };
@@ -55,15 +57,9 @@ store.dispatch(addDrink('kombucha'));
 
 store.dispatch(removeDrink('water'));
 
-store.dispatch({
-  type: 'ADD_CHIPS',
-  payload: 'all-dressed'
-});
+store.dispatch(addChips('all-dressed'));
 
-store.dispatch({
-  type: 'ADD_CHIPS',
-  payload: 'fritos'
-});
+store.dispatch(addChips('fritos'));
 
 store.dispatch({
   type: 'ADD_SANDWICH',
@@ -80,16 +76,11 @@ store.dispatch({
   payload: 'cheese'
 });
 
-store.dispatch({
-  type: 'REMOVE_CHIPS',
-  payload: 'all-dressed'
-});
+store.dispatch(removeChips('fritos'));
 
 store.dispatch(emptyDrinks());
 
-store.dispatch({
-  type: 'EMPTY_CHIPS'
-});
+store.dispatch(emptyChips());
 
 store.dispatch({
   type: 'EMPTY_SANDWICHES'
