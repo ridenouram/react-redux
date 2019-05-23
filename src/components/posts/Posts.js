4ad4a1d9a7b436ec64befe3f2f814fed545b52f3
Anna Ridenour <annamridenour@gmail.com>
2019-05-23 16:40:08 -0700

    refactored postSelectors to return array, post objects include id

---
+++ src/components/posts/Posts.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Post from './Post';
+
+function Posts({ posts, deletePost }) {
+  const postList = posts.map(post => (
+    <li key={post.id}>
+      <Post deletePost={deletePost} post={post} />
+    </li>
+  ));
+
+  return (
+    <ul>
+      {postList}
+    </ul>
+  );
+}
+
+Posts.propTypes = {
+  posts = 
+}
--- src/selectors/postSelectors.test.js
+++ src/selectors/postSelectors.test.js
@@ -1,4 +1,4 @@
-import { getAllPosts, getPost } from "./postSelectors";
+import { getAllPosts, getPost } from './postSelectors';
 
 describe('post selectors', () => {
   it('gets all posts', () => {
@@ -34,21 +34,13 @@ describe('post selectors', () => {
       }
     };
 
-    expect(getAllPosts(state)).toEqual({
-      '555': {
-        title: 'hi there',
-        body: 'why does it shake'
-      },
-      '666': {
-        title: 'hola',
-        body: 'the body the body'
-      },
-      '777': {
-        title: 'merhaba',
-        body: 'why does it move'
-      }
-    });
+    expect(getAllPosts(state)).toEqual(
+      [{ body: 'why does it shake', id: '555', title: 'hi there' }, 
+        { body: 'the body the body', id: '666', title: 'hola' }, 
+        { body: 'why does it move', id: '777', title: 'merhaba' }]
+    );
   });
+
   it('gets a post by ID', () => {
     const state = {
       posts: {
@@ -83,6 +75,7 @@ describe('post selectors', () => {
     };
 
     expect(getPost(state, '666')).toEqual({
+      id: '666',
       title: 'hola',
       body: 'the body the body'
     });
