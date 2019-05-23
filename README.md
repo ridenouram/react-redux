4116cd37205b47d1e8100e91221b438951564198
Anna Ridenour <annamridenour@gmail.com>
2019-05-23 11:50:41 -0700

    wrote post selectors, tests passing

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
-* create a blog-combine-reducers branch (branch from blog-reducers)
-* create src/actions/commentActions.js
-  - create an action to create a comment for a post (by post index)
-  - create an action to delete a comment for a post (by post index and comment index)
-* create src/reducers/commentReducer.js
-  - store comments for each post (create an object where the key is a posts index) and the value is an array of comments
-  - handle create a comment
-  - handle delete a comment
-  - delete post should also delete all comments
-**refactor with shortid
+* create a blog-selectors branch (branch from blog-combine-reducers)
+* create src/selectors/postSelectors.js
+  - get all posts
+  - get a post by index
+* create src/selectors/commentSelectors.js
+  - get all comments by post
---
+++ src/selectors/postSelectors.test.js
@@ -0,0 +1,90 @@
+import { getAllPosts, getPost } from "./postSelectors";
+
+describe('post selectors', () => {
+  it('gets all posts', () => {
+    const state = {
+      posts: {
+        '555': {
+          title: 'hi there',
+          body: 'why does it shake'
+        },
+        '666': {
+          title: 'hola',
+          body: 'the body the body'
+        },
+        '777': {
+          title: 'merhaba',
+          body: 'why does it move'
+        }
+      },
+      comments: {
+        '555': {
+          'AAA': {
+            body: 'the fear'
+          },
+          'BBB': {
+            body: 'great song'
+          }
+        },
+        '666': {
+          'CCC': {
+            body: 'yes wow'
+          }
+        }
+      }
+    };
+
+    expect(getAllPosts(state)).toEqual({
+      '555': {
+        title: 'hi there',
+        body: 'why does it shake'
+      },
+      '666': {
+        title: 'hola',
+        body: 'the body the body'
+      },
+      '777': {
+        title: 'merhaba',
+        body: 'why does it move'
+      }
+    });
+  });
+  it('gets a post by ID', () => {
+    const state = {
+      posts: {
+        '555': {
+          title: 'hi there',
+          body: 'why does it shake'
+        },
+        '666': {
+          title: 'hola',
+          body: 'the body the body'
+        },
+        '777': {
+          title: 'merhaba',
+          body: 'why does it move'
+        }
+      },
+      comments: {
+        '555': {
+          'AAA': {
+            body: 'the fear'
+          },
+          'BBB': {
+            body: 'great song'
+          }
+        },
+        '666': {
+          'CCC': {
+            body: 'yes wow'
+          }
+        }
+      }
+    };
+
+    expect(getPost(state, '666')).toEqual({
+      title: 'hola',
+      body: 'the body the body'
+    });
+  });
+});
