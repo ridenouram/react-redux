05b3369c875bf53807bd5ab939cfe246dd8959f5
Anna Ridenour <annamridenour@gmail.com>
2019-05-23 09:56:51 -0700

    combineReducer created and working

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
-import reducer from './reducers/postReducer';
+import reducer from './reducers';
 import { createPost, deletePost, updatePost } from './actions/postActions';
+import { createComment } from './actions/commentAction';
 
 const store = createStore(reducer);
 
@@ -19,6 +20,8 @@ store.dispatch(createPost('my title', 'post body', '777'));
 store.dispatch(deletePost('666'));
 
 store.dispatch(updatePost('777', 'updated body'));
+
+store.dispatch(createComment('777', 'ABC', 'nice post!'));
 
 render(
   <p>REDUX</p>,
