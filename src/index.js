8fa169be4d32ac75629bc2a5568d9fd08e96b35d
Anna Ridenour <annamridenour@gmail.com>
2019-05-22 17:57:24 -0700

    updatePost works!!

--- src/index.js
+++ src/index.js
@@ -2,23 +2,23 @@ import React from 'react';
 import { render } from 'react-dom';
 import { createStore } from 'redux';
 import reducer from './reducers/postReducer';
-import { createPost, deletePost } from './actions/postActions';
+import { createPost, deletePost, updatePost } from './actions/postActions';
 
 const store = createStore(reducer);
 
 
-console.log('before all', store.getState());
 
 store.subscribe(() => {
   console.log(store.getState());
 });
 
 store.dispatch(createPost('my title', 'post body', '666'));
-console.log('666 added', store.getState());
+
 store.dispatch(createPost('my title', 'post body', '777'));
-console.log('666 and 777', store.getState());
+
 store.dispatch(deletePost('666'));
-console.log('666 removed', store.getState());
+
+store.dispatch(updatePost('777', 'updated body'));
 
 render(
   <p>REDUX</p>,
--- src/reducers/postReducer.js
+++ src/reducers/postReducer.js
@@ -1,4 +1,4 @@
-import { CREATE_POST, DELETE_POST } from '../actions/postActions';
+import { CREATE_POST, DELETE_POST, UPDATE_POST } from '../actions/postActions';
 
 const initialState = {
   posts: {}
@@ -16,12 +16,22 @@ function addProperty(obj, key, value) {
   return newObj;
 }
 
+function updatePostBody(obj, key, newBody) {
+  const newObj = { ...obj };
+  let post = newObj[key];
+  post.body = newBody;
+  newObj[key] = post;
+  return newObj;
+}
+
 export default function reducer(state = initialState, action) {
   switch(action.type) {
     case CREATE_POST:
       return { ...state, posts: addProperty(state.posts, action.payload.id, { title: action.payload.title, body: action.payload.body }) };
     case DELETE_POST:
       return { ...state, posts: removeProperty(state.posts, action.payload) };
+    case UPDATE_POST:
+      return { ...state, posts: updatePostBody(state.posts, action.payload.id, action.payload.body) };
     default:
       return state;  
   }
